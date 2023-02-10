c01fa7464892ac773ea0b3946dcf983007d785b0
NathanielMention <nathanielmention@gmail.com>
2023-02-10 15:48:10 -0500

    add list and random position

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ WINDOW = 1000
 TILE_SIZE = 50
 # tuple that defines range for rand coordinates
 RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
-get_random_position = Lambda: [randrange(*RANGE), randrange(*RANGE)]
+def get_random_position(): return [randrange(*RANGE), randrange(*RANGE)]
+
+
 snake = pg.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE - 2])
 snake.center = get_random_position()
 length = 1
@@ -23,5 +25,7 @@ while True:
         if event.type == pg.QUIT:
             exit()
     screen.fill('black')
+    # draw snake
+    [pg.draw.rect(screen, 'green', segment) for segment in segments]
     pg.display.flip()
     clock.tick(60)
