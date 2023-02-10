9f38d33ecb2752079100ff002a8a5f168c68f436
NathanielMention <nathanielmention@gmail.com>
2023-02-10 15:57:27 -0500

    handle change direction

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ snake = pg.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE - 2])
 snake.center = get_random_position()
 length = 1
 segments = [snake.copy()]
+snake_dir = (0, 0)
 screen = pg.display.set_mode([WINDOW] * 2)
 # instance of clock class to set frame rate
 clock = pg.time.Clock()
@@ -24,6 +25,16 @@ while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
+        # handle change direction keys
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_w:
+                snake_dir = (0, -TILE_SIZE)
+            if event.key == pg.K_s:
+                snake_dir = (0, TILE_SIZE)
+            if event.key == pg.K_a:
+                snake_dir = (-TILE_SIZE, 0)
+            if event.key == pg.K_d:
+                snake_dir = (TILE_SIZE, 0)
     screen.fill('black')
     # draw snake
     [pg.draw.rect(screen, 'green', segment) for segment in segments]
