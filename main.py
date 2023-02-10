1d8a13a9465ca143d5f5185cf72717730f8df92f
NathanielMention <nathanielmention@gmail.com>
2023-02-10 15:33:12 -0500

    add tiles and range tuple for window

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from random import randrange
 
 # create window/screen for game to show up on
 WINDOW = 1000
+# since game is in form of grid break into tiles
+TILE_SIZE = 50
+# tuple that defines range for rand coordinates
+RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
+get_random_position = Lambda: [randrange(*RANGE), randrange(*RANGE)]
+snake = pg.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE - 2])
+snake.center = get_random_position()
+length = 1
+segments = [snake.copy()]
 screen = pg.display.set_mode([WINDOW] * 2)
 # instance of clock class to set frame rate
 clock = pg.time.Clock()
