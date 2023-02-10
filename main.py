adca8b26dad972a80f705ceebdf15d9d9613b1fd
NathanielMention <nathanielmention@gmail.com>
2023-02-10 14:51:09 -0500

    setup game window/handle exit

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import pygame as pg
 from random import randrange
+
+# create window/screen for game to show up on
+WINDOW = 1000
+screen = pg.display.set_mode([WINDOW] * 2)
+# instance of clock class to set frame rate
+clock = pg.time.Clock()
+
+# main loop of program
+while True:
+    # check for closing of program
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit()
+    screen.fill('black')
+    pg.display.flip()
+    clock.tick(60)
